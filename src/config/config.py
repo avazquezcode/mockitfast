7109d042e31d193fd7ff6ab9ec64296c1f136827
avazquezcode <avazquezcode@gmail.com>
2024-12-14 02:21:22 +0100

    Improve code readability and structure for config

--- src/config/config.py
+++ src/config/config.py
@@ -2,25 +2,40 @@ import os
 
 ENV_TRUE_VALUES = ('true', '1')
 
+# defaults
+DEFAULT_HOST = "0.0.0.0"
+DEFAULT_PORT = "3000"
+DEFAULT_WATCHER_INTERVAL = "1"
+DEFAULT_HEALTH_CHECK_PATH = "/health/check"
+
 
 class Config:
     def __init__(self):
         #  Server configs
-        self.host = os.environ.get("HOST", default="0.0.0.0")
-        self.port = int(os.environ.get("PORT", default="3000"))
+        self.host = os.getenv("HOST", default=DEFAULT_HOST)
+        self.port = load_int("PORT", default=DEFAULT_PORT)
 
         #  Router configs
-        self.router_config_path = os.environ.get("ROUTER_CONFIG_PATH")
+        self.router_config_path = os.getenv("ROUTER_CONFIG_PATH")
 
         # File watcher configs
-        self.watcher_interval_seconds = int(
-            os.environ.get("WATCHER_INTERVAL_SECONDS", default=1))
+        self.watcher_interval_seconds = load_int(
+            "WATCHER_INTERVAL_SECONDS", default=DEFAULT_WATCHER_INTERVAL)
 
-        #  Health check configs
-        health_check_enabled_val = os.getenv(
-            "HEALTH_CHECK_ENABLED",
-            default="False"
-        ).lower()
-        self.is_health_check_enabled = health_check_enabled_val in ENV_TRUE_VALUES
-        self.health_check_path = os.environ.get(
-            "HEALTH_CHECK_PATH", default="/health/check")
+        # Health check configs
+        self.is_health_check_enabled = load_bool("HEALTH_CHECK_ENABLED")
+        self.health_check_path = os.getenv(
+            "HEALTH_CHECK_PATH", default=DEFAULT_HEALTH_CHECK_PATH)
+
+
+@staticmethod
+def load_int(env_var, default):
+    try:
+        return int(os.getenv(env_var, default))
+    except ValueError:
+        raise ValueError(f"{env_var} must be an integer")
+
+
+@staticmethod
+def load_bool(env_var, default="False"):
+    return os.getenv(env_var, default).lower() in ENV_TRUE_VALUES
